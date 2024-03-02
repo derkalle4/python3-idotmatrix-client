620dbba2c1eba0bb9fe970e95c4dfa58ac358bf7
yair-mantis <yair@mantis-vision.com>
2024-03-02 19:00:11 +0200

    adapt .sh instead of ps1

--- create_venv.sh
+++ create_venv.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 set -e
-# get the directory of file
+# get the directory of the file
 IDO_DIR="$(dirname "$0")"
+
+# Check if Python is available as 'python3' or 'python'
+if command -v python3 &> /dev/null
+then
+    PYTHON_CMD=python3
+else
+    PYTHON_CMD=python
+fi
+
 # create venv
-python3 -m venv "$IDO_DIR/venv"
+$PYTHON_CMD -m venv "$IDO_DIR/venv"
+
 # enable venv
-source "$IDO_DIR/venv/bin/activate"
+# Check if the OS is Windows and choose the correct activation script
+if [[ "$OSTYPE" == "msys" ]]; then
+    # Windows Git Bash
+    source "$IDO_DIR/venv/Scripts/activate"
+else
+    # POSIX (Linux, macOS, etc.)
+    source "$IDO_DIR/venv/bin/activate"
+fi
+
 # install dependencies from pyproject.toml
-pip3 install "$IDO_DIR/"
+$PYTHON_CMD -m pip install "$IDO_DIR/"
