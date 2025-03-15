171d2281230b8775554456b003fcebcbc0468d60
miramiram <199418076+miramiram@users.noreply.github.com>
2025-03-15 15:19:00 +0100

    Remove moved files

--- gui.ps1
+++
@@ -1,4 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-.\venv\Scripts\Activate.ps1
-python3 gui.py
-pause  # This prevents the terminal from closing when Python closes. Its useful for reading errors without opening a new terminal, but it will leave the terminal open when the script is opened with a hidden window.
