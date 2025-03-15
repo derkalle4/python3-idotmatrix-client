2c60fd504cc896d8e597ac518651478668997640
miramiram <199418076+miramiram@users.noreply.github.com>
2025-03-15 06:38:45 +0100

    Fix icon & use more secure pathing
    Now fetches the script launch directory from a built in variable, and uses that everywhere except for the invocation of the VENV.

--- setup_and_make_launcher.ps1
+++ setup_and_make_launcher.ps1
@@ -2,20 +2,23 @@
 echo "Setting up iDotMatrix..."
 echo "(If something goes wrong, try running the script with an administrator instance of Powershell)"
 
-if (Test-Path -Path '.\gui.py') {
-    echo "gui.py found, assuming the script has launched from the correct path."
+$root = $PSScriptRoot
+echo "Launching from: $root"
+
+if (Test-Path -Path "$root\gui.py") {
+	echo "gui.py found, assuming the script has launched from the correct path."
 } else {
-    echo "`nERROR: gui.py not found!`nThis means that the script wasn't launched from the correct folder.`n"
-    echo "To work correctly, you need to launch this script from the root folder of the iDotMatrix git folder."
-    echo "To do this, open the folder this script is located in in Windws Explorer, right click the script, and press 'Run in powershell'.`n"
-    echo "If that doesn't work, either open a powershell window and cd to the folder, or open the iDotMatrix git folder in Windows Explorer, then shift-right-click inside in an empty spot in the folder window, and click 'Open in powershell'."
+	echo "`nERROR: gui.py not found!`nThis means that the script wasn't launched from the correct folder.`n"
+		echo "To work correctly, you need to launch this script from the root folder of the iDotMatrix git folder."
+		echo "To do this, open the folder this script is located in in Windws Explorer, right click the script, and press 'Run in powershell'.`n"
+		echo "If that doesn't work, either open a powershell window and cd to the folder, or open the iDotMatrix git folder in Windows Explorer, then shift-right-click inside in an empty spot in the folder window, and click 'Open in powershell'."
     echo "With powershell open, write .\\setup_and_make_launcher.ps1 and press enter"
     pause
     exit
 }
 
 echo "This script will now open the VENV and install the required PIP dependencies."
-if (Test-Path -Path '.\venv\Scripts\Activate.ps1') {
+if (Test-Path -Path "$root\venv\Scripts\Activate.ps1") {
     echo "Venv set up, opening it."
 } else {
     echo "Venv not set up, creating it. Make sure you have python installed."
@@ -34,16 +37,17 @@ echo "`nThis script will now create the shortcut on your desktop."
 
 $WshShell = New-Object -COMObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$Home\Desktop\iDotMatrix GUI.lnk")
-$Shortcut.TargetPath = '%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe'
+$Shortcut.TargetPath = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
 
 $userInput = Read-Host -Prompt "`nDo you want the terminal to be hidden when launching the GUI?`n> [y/n]"
 if ($userInput -eq "y") {
-	$Shortcut.Arguments = '-WindowStyle Hidden -File ".\run.ps1"'  
+	$Shortcut.Arguments = "-WindowStyle Hidden -File `"$root\run.ps1`""
 	echo "If the GUI isn't opening, re-run this script without hiding the terminal, so you can see what went wrong."
 } else {
-	$Shortcut.Arguments = '-File ".\run.ps1"'  #-WindowStyle Hidden 
+	$Shortcut.Arguments = "-File `"$root\run.ps1`""  #-WindowStyle Hidden 
 }
 
+$Shortcut.IconLocation = "$root\idmc.ico"
 $Shortcut.WorkingDirectory = split-path -parent $MyInvocation.MyCommand.Definition
 $Shortcut.Save()
 echo "`n--------`nA shortcut should now have been created on your desktop. `nIf some commands in the script fails, first re-run without a hidden terminal if you chose to hide it, then make sure you have Python installed, and see if you can open the GUI manually through powershell, by manually using the commands in this file."
