84ef91491141974449c4205950db157bc33c7843
Mira <199418076+miramiram@users.noreply.github.com>
2025-03-21 03:35:56 +0100

    Ensure venv python binary is used

--- run_in_venv.sh
+++ run_in_venv.sh
@@ -2,16 +2,22 @@
 set -e
 # get directory of file
 IDO_DIR="$(dirname "$0")"
-# activate venv
+
+# ensure we have the right global python
+. "$IDO_DIR"/find_cmds.sh
+
+# enable venv
+# Check if the OS is Windows and choose the correct activation script
 if [[ "$OSTYPE" == "msys" ]]; then
     # Windows Git Bash
     source "$IDO_DIR/venv/Scripts/activate"
+    PYTHON_CMD_VENV="$IDO_DIR/venv/Scripts/$PYTHON_CMD"
 else
     # POSIX (Linux, macOS, etc.)
     source "$IDO_DIR/venv/bin/activate"
+    PYTHON_CMD_VENV="$IDO_DIR/venv/bin/$PYTHON_CMD"
 fi
-
-. "$IDO_DIR"/find_cmds.sh
+# todo: Check for binaries similar to find_cmds. A venv can be created with python3 but only contain pytgom on some systems
 
 # run app
-$PYTHON_CMD "$IDO_DIR/app.py" "$@"
+$PYTHON_CMD_VENV "$IDO_DIR/app.py" "$@"
