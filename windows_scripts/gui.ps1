6e0277f16f94c4041f5c39c2f7f25f9ead42dea6
miramiram <199418076+miramiram@users.noreply.github.com>
2025-03-21 20:11:03 +0100

    Use run_in_venv

--- windows_scripts/gui.ps1
+++ windows_scripts/gui.ps1
@@ -1,10 +1,4 @@
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-$root = "$PSScriptRoot\.."
-. "$root\venv\Scripts\Activate.ps1"
-if (-not $?){
-	Write-Host "ERROR: Failed to open venv, try running the setup script in the /windows_scripts/ of the git folder."
-	exit
-}
-cd $root  # Python needs to open in the root of the git folder for relative paths to be correct.
-python ".\gui.py"
+$root = Resolve-Path -Path "$PSScriptRoot\.."
+& "$root\run_in_venv.ps1" -DontInvokeApp "$root\gui.py"
 pause
