f99854dd85291d0da7bfea6eb98951490806c734
miramiram <199418076+miramiram@users.noreply.github.com>
2025-03-21 20:06:34 +0100

    Check directory before running

--- run_in_venv.ps1
+++ run_in_venv.ps1
@@ -1,15 +1,28 @@
 $originalArgs = $args
 $originalPath = Get-Location
 & {
-	Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-	$root = $PSScriptRoot
-	. "$root\venv\Scripts\Activate.ps1"
-	if (-not "$?"){
-		Write-Host "ERROR: Failed to open venv, try running the setup script in the /windows_scripts/ of the git folder."
-		exit
-		pause
-	}
-	Set-Location -Path $root
-	python "$root\app.py" $originalArgs
-	Set-Location -Path $originalPath
+    $root = $PSScriptRoot
+    Set-Location -Path "$root"
+
+    $likelyCorrectPath = Test-Path -Path "$root\app.py" -PathType Leaf
+    if (-not $likelyCorrectPath){
+        Write-Host "ERROR: The script seems to have been run from the wrong path. Try right-clicking the .ps1 file and clicking 'run with powershell'."
+        Set-Location -Path $originalPath
+        pause
+        exit
+    }
+    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
+    . "$root\venv_windows\Scripts\Activate.ps1"
+    if (-not "$?"){
+        Write-Host "ERROR: Failed to open venv. Make sure Python is installed, then try this script again."
+        Set-Location -Path $originalPath
+        pause
+        exit
+    }
+
+    $py_cmd = "python"
+        & $py_cmd "$root/app.py" $originalArgs
+
+    Set-Location -Path $originalPath
+
 }
