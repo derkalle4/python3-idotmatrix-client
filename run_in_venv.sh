43a93a96bb5a99b293db62f5c4c8bb036271a6ae
miramiram <199418076+miramiram@users.noreply.github.com>
2025-03-21 15:00:04 +0100

    Refactor script to use functions

--- run_in_venv.sh
+++ run_in_venv.sh
@@ -1,17 +1,36 @@
 #!/usr/bin/env bash
 set -e
-# get directory of file
-IDO_DIR="$(dirname "$0")"
-# activate venv
-if [[ "$OSTYPE" == "msys" ]]; then
-    # Windows Git Bash
-    source "$IDO_DIR/venv/Scripts/activate"
-else
-    # POSIX (Linux, macOS, etc.)
-    source "$IDO_DIR/venv/bin/activate"
-fi
 
-. "$IDO_DIR"/find_cmds.sh
+get_py() {
+    if command -v python3 &>/dev/null; then
+        echo "python3"
+        return 0
+    fi
+    if command -v python &>/dev/null; then
+        echo "python"
+        return 0
+    fi
+    echo "Python not found" >&2
+    return 1
+}
 
+get_venv_dir() {
+    local pwd_dir="$1"
+    if [[ "$OSTYPE" == "msys" ]]; then
+        # Windows Bash (msys, git bash)
+        echo "$pwd_dir/venv/Scripts"
+        return 0
+    else
+        # POSIX (Linux, macOS, etc.)
+        echo "$pwd_dir/venv/bin"
+        return 0
+    fi
+}
+
+
+pwd_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)  # POSIX-safe script directory fetching. Details: https://stackoverflow.com/a/29835459
+py_cmd=$(get_py)
+# open venv
+source "$venv_dir/activate"
 # run app
-$PYTHON_CMD "$IDO_DIR/app.py" "$@"
+$py_cmd "$pwd_dir/app.py" "$@"
