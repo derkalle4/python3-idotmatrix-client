e2b4302778182322d0b86e554e4dea1e72876172
miramiram <199418076+miramiram@users.noreply.github.com>
2025-03-21 15:05:06 +0100

    Ensure venv's python binary is used
    Depending on the system and Python version, calling the python command directly when inside the venv can fail to use the venv's python binary, instead using the global one.

--- run_in_venv.sh
+++ run_in_venv.sh
@@ -27,10 +27,25 @@ get_venv_dir() {
     fi
 }
 
+get_venv_py() {
+    # find and return a direct path to the python binary in the venv.
+    # this ensures we're not calling the global python binary,
+    # which can sometimes happen depending on the system.
+    local venv_dir="$1"
+    test -f "$venv_dir/python3" && echo "$venv_dir/python3" && return 0
+    test -f "$venv_dir/python"  && echo "$venv_dir/python"  && return 0
+    test -f "$venv_dir/py3"     && echo "$venv_dir/py3"     && return 0
+    test -f "$venv_dir/py"      && echo "$venv_dir/py"      && return 0
+    echo "Python not found in venv, was it correctly set up?" >&2
+    return 1
+}
+
 
 pwd_dir=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd -P)  # POSIX-safe script directory fetching. Details: https://stackoverflow.com/a/29835459
 py_cmd=$(get_py)
 # open venv
 source "$venv_dir/activate"
+# make sure we use the python binary belonging to the venv
+venv_py_cmd=$(get_venv_py "$venv_dir")
 # run app
-$py_cmd "$pwd_dir/app.py" "$@"
+$venv_py_cmd "$pwd_dir/app.py" "$@"
